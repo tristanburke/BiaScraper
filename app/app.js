3bc9652e74f07413dc81ae0b1672a769b0b99703
ian <ian.c.mcdonald5@gmail.com>
2017-07-09 17:55:50 -0700

    starting the real work

--- app/app.js
+++ app/app.js
@@ -1,10 +1,19 @@
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
+"use strict";
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const bodyParser = require("body-parser");
+const expressSanitizer = require("express-sanitizer");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/news_app");
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 app.get("/", function (req, res) {
     res.render("index.ejs");
