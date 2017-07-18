909f042ded56e4fcd5fe2e3df5a1e285e2e181e9
Tristan Burke <tristan_burke@berkeley.edu>
2017-07-17 19:07:29 -0700

    added angular and updated app.js to account for html

--- app/app.js
+++ app/app.js
@@ -14,14 +14,14 @@ const app = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/news_app");
-app.set("view engine", "html");
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
-
 app.get("/", function (req, res) {
-    res.render("index.html", {results: null});
+    res.render("index.html");
 });
 
 app.post("/", function (req, res) {
@@ -29,15 +29,15 @@ app.post("/", function (req, res) {
 
     let options = {
         mode: 'text',
-        pythonPath: '/home/ian/.virtualenvs/spacy/bin/python',
-        scriptPath: '/home/ian/Programming/projects/BiaScraper/app/',
+        pythonPath: '/home/tristan/.virtualenvs/spacy/bin/python',
+        scriptPath: '/home/tristan/Programming/projects/BiaScraper/app/',
         args: [article]
     };
 
     pythonShell.run('get_matches.py', options, function (err, results) {
         if (err) throw err;
         // results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
+        console.log('results: %jpw', results);
         res.render("index.html", {results: results});
     });
 
