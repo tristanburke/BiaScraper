8813947d8eaf97293953c3eaeda1aabdfa1d1ae6
ian <ian.c.mcdonald5@gmail.com>
2017-07-23 17:29:09 -0700

    about to push to server

---
+++ app/controllers/main_controller.js
@@ -0,0 +1,19 @@
+mainApp.controller("studentController", function($scope) {
+    $scope.retrieve = function(){
+        var article = $scope.url;
+        var options = {
+            mode: 'text',
+            pythonPath: '/home/ian/.virtualenvs/BiaScraper/bin/python',
+            scriptPath: '/home/ian/Programming/projects/BiaScraper/app/',
+            args: [article]
+        };
+        pythonShell.run('get_matches.py', options, function (err, results) {
+            if (err) throw err;
+            // results is an array consisting of messages collected during execution
+            console.log('results: %j', results);
+            res.render("index.ejs", {results: results});
+        });
+    };
+    $scope.retrieve();
+    $scope.results = {};
+});
